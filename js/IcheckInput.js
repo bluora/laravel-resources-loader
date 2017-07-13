1576ec6b2d9d870761f459f453c0836a49ca9a22
Rocco Howard <rocco@hnh.digital>
2017-07-13 06:54:15 +0000

    Only apply if we have not already

--- js/IcheckInput.js
+++ js/IcheckInput.js
@@ -1,12 +1,15 @@
 
 window.extensions_initializer['init-icheck-input'] = function() {
-    if (!($(this).parent().hasClass('config-icheck-input_flat-green')
-        || $(this).parent().hasClass('iradio_flat-green'))) {
-        $(this).addClass('icheck');
-        $(this).addClass('icheckbox_flat-green');
-        $(this).iCheck({
-            checkboxClass: 'icheckbox_flat-green',
-            radioClass: 'iradio_flat-green'
-        });
+    if (!$(this).hasClass('icheck-applied')) {
+        if (!($(this).parent().hasClass('config-icheck-input_flat-green')
+            || $(this).parent().hasClass('iradio_flat-green'))) {
+            $(this).addClass('icheck');
+            $(this).addClass('icheckbox_flat-green');
+            $(this).iCheck({
+                checkboxClass: 'icheckbox_flat-green',
+                radioClass: 'iradio_flat-green'
+            });
+            $(this).addClass('icheck-applied');
+        }
     }
 }
